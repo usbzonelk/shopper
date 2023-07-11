d206474886dddb06d4051d417c628017b4e86383
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-07-11 23:44:03 +0530

    Added first #Product schema

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,19 @@
 const mongoUSER = "bartyslr";
 const mongoPass = "W4MZeyrsSEFJnilc";
+const dbName = "shopper";
 
 const mongoose = require("mongoose");
 
 async function connectToDatabase() {
   try {
     await mongoose.connect(
-      `mongodb+srv://${mongoUSER}:${mongoPass}@cluster69.7tz3qnk.mongodb.net/?retryWrites=true&w=majority`,
+      `mongodb+srv://${mongoUSER}:${mongoPass}@cluster69.7tz3qnk.mongodb.net/${dbName}?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
     );
+
     console.log("Connected to the database");
   } catch (error) {
     console.error("Error connecting to the database:", error.message);
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,1 +1,18 @@
- 
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+/* productSchema.methods.speak = function speak() {
+  const greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+};
+ */
+const laptop = new Product({ name: "Laptop" });
+
+await laptop.save();
