f9d671b8be025ab7a5dae06afa21144158e994ec
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-07-19 18:09:29 +0530

    Added functiond for static schema generation

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,9 +1,15 @@
+/*
+Product creation method:
+ 1. create new productType by giving its exclusive field values
+ 2. when adding new product of that product type, inject the common values 
+    when adding the new product
+*/
+
 const mongoose = require("mongoose");
 
 const productTypesSchema = new mongoose.Schema({
   adminId: { type: String, required: true },
-  slug: { type: String, required: true },
-
+  slugtype: { type: String, required: true },
   fields: [
     {
       name: { type: String, required: true },
@@ -16,26 +22,44 @@ const ProductType = mongoose.model("producttypes", productTypesSchema);
 
 /////
 
-const productSchema = new mongoose.Schema({
-  title: String,
-  slug: String,
-  price: Number,
-  discount: Number,
-  instock: Number,
-  description: String,
-  warrantyMonths: Number,
-  type: String,
-  coverPhoto: String,
-  photos: [String],
-});
-
-productSchema.methods.speak = function speak() {
+/* productSchema.methods.speak = function speak() {
   const greeting = this.name
     ? "Meow name is " + this.name
     : "I don't have a name";
   console.log(greeting);
-};
+}; */
 
 const Product = mongoose.model("Product", productSchema);
 
 await laptopSchema.save();
+
+const saveNewProductType = async (adminID, typeName, newTypeProperties) => {
+  const newType = new ProductType({
+    adminID: adminID,
+    slugtype: typeName,
+    fields: newTypeProperties,
+  });
+  await newType.save();
+};
+
+const saveNewProduct = async (productTypeAttributes, productDetails) => {
+  const newProductProperties = {
+    title: String,
+    slug: String,
+    price: Number,
+    discount: Number,
+    instock: Number,
+    description: String,
+    warrantyMonths: Number,
+    type: String,
+    coverPhoto: String,
+    photos: [String],
+  };
+  const productSchema = new mongoose.Schema(newProductProperties);
+  const Product = mongoose.model("Product", productSchema);
+  const newProduct = new Product(productDetails);
+  await newProduct.save();
+};
+
+module.exports = saveNewProductType;
+module.exports = saveNewProduct;
