56cda09c104d171c870d617b7e3f64d9f5b12037
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-07-31 13:22:29 +0530

    Added Add/Remove cart logic

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,1 +1,127 @@
- 
+const mongoose = require("mongoose");
+
+const cartManager = {
+  cartSchema: function () {
+    return new mongoose.Schema({
+      email: {
+        type: String,
+        required: true,
+      },
+      items: [
+        {
+          product: {
+            type: String,
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            default: 1,
+          },
+          discount: {
+            type: Number,
+            default: 0,
+          },
+        },
+      ],
+      addedAt: {
+        type: Date,
+        default: Date.now,
+      },
+    });
+  },
+
+  cartModel: function () {
+    return mongoose.model("carts", this.cartSchema());
+  },
+
+  generateNewItem: async function (slug, qty, discount) {
+    const itemInfo = {
+      product: slug,
+      quantity: qty,
+      discount: discount,
+    };
+    return itemInfo;
+  },
+
+  createNewCart: async function (email, products) {
+    const newCartDetails = {
+      email: email,
+      items: products,
+      addedAt: new Date(),
+    };
+    const Cart = this.cartModel(newCartDetails);
+
+    const newCart = new Cart(newCartDetails);
+
+    const savedCart = await newCart.save();
+
+    return savedCart;
+  },
+
+  getAllCarts: async function () {
+    try {
+      const cartSchema = this.cartModel();
+      const allCarts = await cartSchema.find({});
+      return allCarts;
+    } catch (err) {
+      return err;
+    }
+  },
+
+  getOneCart: async function (params) {
+    const cartSchema = this.cartModel();
+    const getMatchedCart = await cartSchema.findOne(params);
+    return getMatchedCart;
+  },
+
+  getManyCarts: async function (params) {
+    const cartSchema = this.cartModel();
+    const getMatchedCarts = await cartSchema.findOne(params);
+    return getMatchedCarts;
+  },
+
+  editOneCart: async function (email, newProductTypeInfo) {
+    const productTypeSchema = this.productTypeModel();
+    const updatedProductType = await productTypeSchema.findOneAndUpdate(
+      email,
+      newProductTypeInfo,
+      {
+        new: true,
+      }
+    );
+
+    return updatedProductType;
+  },
+
+  addProductToCart: async function (mail, newItem) {
+    const cartSchema = this.cartModel();
+    const updatedCart = await cartSchema.updateOne(
+      { email: mail },
+      { $push: { items: newItem } }
+    );
+    return updatedCart;
+  },
+
+  removeItemFromCart: async function (mail, itemToRemove) {
+    const cartSchema = this.cartModel();
+    const updatedCart = await cartSchema.updateOne(
+      { email: mail },
+      { $pull: { items: itemToRemove } }
+    );
+    return updatedCart;
+  },
+
+  deleteOneCart: async function (params) {
+    const cartSchema = this.cartModel();
+    const deletedCart = await cartSchema.deleteOne(params);
+    return deletedCart;
+  },
+
+  deleteManyCarts: async function (params) {
+    const cartSchema = this.cartModel();
+    const deletedCarts = await cartSchema.deleteMany(params);
+    return deletedCarts;
+  },
+};
+
+module.exports = { cartManager };
