cb6e0a8b44167e5914edf8c2281f7e624f56f233
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-08-15 13:51:42 +0530

    Added functions related to password, email changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/Users");
+const bcrypt = require("../utils/bcrypt");
 const users = User.usersManager;
 
 const createTempUser = async (email, password) => {
@@ -70,4 +71,64 @@ const deactivateUser = async (email) => {
   return outputMsg;
 };
 
-module.exports = { createTempUser, verifyUser, deactivateUser };
+const passwordReset = async (email, newPass) => {
+  let userEdited = null;
+  const outputMsg = {};
+
+  try {
+    const mailValidity = await users.getOneUserInfo({ email: email });
+    if (!mailValidity) {
+      return new Error((message = "Account doesn't exist"));
+    }
+    const newPassHashed = await bcrypt.hashPassword(newPass);
+    userEdited = await users.editOneUser(
+      { email: email },
+      { password: newPassHashed.pass }
+    );
+    outputMsg.user = { email: userEdited.email, status: userEdited.status };
+    outputMsg.success = true;
+    outputMsg.message = "Successfully changed password";
+  } catch (error) {
+    outputMsg.success = false;
+    outputMsg.message = "Error occured";
+    outputMsg.error = error.message;
+  }
+  return outputMsg;
+};
+
+const changeMail = async (oldEmail, newEmail) => {
+  let userEdited = null;
+  const outputMsg = {};
+
+  try {
+    const mailValidity = await users.getOneUserInfo({ email: oldEmail });
+    if (!mailValidity) {
+      return new Error((message = "Account doesn't exist"));
+    }
+    const newMailValidity = await users.getOneUserInfo({ email: newEmail });
+    if (newMailValidity) {
+      return new Error((message = "An account already exists"));
+    }
+
+    userEdited = await users.editOneUser(
+      { email: oldEmail },
+      { email: newEmail }
+    );
+    outputMsg.user = { email: userEdited.email, status: userEdited.status };
+    outputMsg.success = true;
+    outputMsg.message = "Successfully changed email";
+  } catch (error) {
+    outputMsg.success = false;
+    outputMsg.message = "Error occured";
+    outputMsg.error = error.message;
+  }
+  return outputMsg;
+};
+
+module.exports = {
+  createTempUser,
+  verifyUser,
+  deactivateUser,
+  passwordReset,
+  changeMail,
+};
